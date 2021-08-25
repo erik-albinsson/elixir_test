a7e4b1affc1462a26a6fa134bcca4e58feb59de4
erikalbinsson02 <erik.albinsson@elev.ga.ntig.se>
2021-08-25 10:26:28 +0200

    gjort övningar -7

--- lib/intro.ex
+++ lib/intro.ex
@@ -1,5 +1,30 @@
 defmodule Intro do
-  def hello do
-    :world
-  end
+  # def smallest(n1,n2) do
+  #   if n1 >= n2 do
+  #     n2
+  # else
+  #   n1
+  # end
+  def smallest(n1,n2) when n1 <= n2, do: n1
+  def smallest(_n1,n2), do: n2
+
+
+def smallest(n1,n2,n3),do: smallest(smallest(n1,n2),n3)
+
+def smallest(n1,n2,n3,n4),do: smallest(smallest(n1,n2),smallest(n3,n4))
+
+def ticketprice(n1) when n1 < 18, do: 10
+def ticketprice(n1) when n1 < 64, do: 20
+def ticketprice(_n1), do: 15
+
+
+def nextnumber(n1), do: n1+1
+
+
+def factorial(input) when input == 0, do: 1
+def factorial(input), do: factorial(input - 1) * input
+
+def fibonacci(input) when input == 0, do: 0
+def fibonacci(input) when input == 1, do: 1
+def fibonacci(input)
 end
--- test/intro_test.exs
+++ test/intro_test.exs
@@ -1,7 +1,41 @@
 defmodule IntroTest do
   use ExUnit.Case
 
-  test "greets the world" do
-    assert Intro.hello() == :world
+  # test "smallest_of_two reverse" do
+  #   assert Intro.smallest(3,1) == 1
+  # end
+
+  # test "smallest_of_two" do
+  #   assert Intro.smallest(1,3) == 1
+  # end
+
+  # test "smallest_of_two negative" do
+  #   assert Intro.smallest(1,-3) == -3
+  # end
+
+  # test "smallest_of_two same" do
+  #   assert Intro.smallest(1,1) == 1
+  # end
+
+  # test "smallest_of_three" do
+  #   assert Intro.smallest_three(1,3,2) == 1
+  # end
+
+  # test "smallest_of_three same" do
+  #   assert Intro.smallest_three(1,1,1) == 1
+  # end
+
+  # test "ticket_price" do
+  #   assert Intro.ticketprice(17) == 10
+  #   assert Intro.ticketprice(19) == 20
+  #   assert Intro.ticketprice(65) == 15
+  # end
+
+  test "fibonacci" do
+    assert Intro.fibonacci(5) == 5
   end
+
+
+
+
 end
